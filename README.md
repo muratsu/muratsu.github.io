cec43257042349c50e7c3aaa2c51df5a5f0fa0c9
Murat Sutunc <sutunc@gmail.com>
2025-04-05 15:41:04 -0700

    move to jekyll

--- README.md
+++
@@ -1,2 +0,0 @@
-My personal blog, powered by Gatsby and based on [the Lumen starter theme](https://github.com/alxshelepenok/gatsby-starter-lumen)
-
--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// Require Katex & Prism
-require('katex/dist/katex.min.css');
-require('prismjs/themes/prism.css');
-
-exports.onClientEntry = () => {};
